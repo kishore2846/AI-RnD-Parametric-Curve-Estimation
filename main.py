dd758a1ce8196688bd1ff8abd88ad31e63b541ef
Lalithkishore <lalithkishore2846@gmail.com>
2026-07-04 00:39:52 +0530

    Visualize input curve

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def load_points(file_path):
@@ -6,17 +7,32 @@ def load_points(file_path):
     return points
 
 
+def plot_curve(points):
+
+    plt.figure(figsize=(8, 6))
+
+    plt.plot(
+        points["x"],
+        points["y"],
+        linewidth=1
+    )
+
+    plt.title("Curve from xy_data.csv")
+    plt.xlabel("X")
+    plt.ylabel("Y")
+
+    plt.grid(True)
+
+    plt.savefig("results/input_curve.png", dpi=300)
+
+    plt.show()
+
+
 def main():
+
     points = load_points("data/xy_data.csv")
 
-    print("\nFirst 5 points:\n")
-    print(points.head())
-
-    print("\nDataset information:\n")
-    print(points.info())
-
-    print("\nBasic statistics:\n")
-    print(points.describe())
+    plot_curve(points)
 
 
 if __name__ == "__main__":
